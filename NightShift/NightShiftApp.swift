5c4ef5638eeef179e9578f8c4e836da6c4cc2972
Nandan Varma <nandanvarma.me@gmail.com>
2026-07-16 10:58:52 -0700

    Initial Commit

---
+++ NightShift/NightShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightShiftApp.swift
+//  NightShift
+//
+//  Created by Nandan Varma Pericharla on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct NightShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
